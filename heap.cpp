618434f5ddbcb28ff4d269a4c3ca795b73ec6547
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-04-09 10:38:33 -0700

    Update heap.cpp

--- heap.cpp
+++ heap.cpp
@@ -14,3 +14,28 @@ int right(int i) {
 int left(int i) {
 	return (2 * i);
 }
+int decreaseKey(HEAP* heap, int pos, float newKey, ptrVertex V) {
+	//int parent = pos / 2;
+	ptrElement temp;
+	//cout << pos << endl;
+	//cout << heap->size << endl;
+	//cout << newKey << endl;
+	//cout << heap->H[pos]->key << endl;
+	if (pos < 1 || pos > heap->size || newKey > heap->H[pos]->key) {
+		//cout << "Error: new key is larger than current key" << endl;
+		return 1;
+	}
+	heap->H[pos]->key = newKey;
+	while ((pos > 1) && (heap->H[parent(pos)]->key >= heap->H[pos]->key)) {
+		temp = heap->H[pos];
+		heap->H[pos] = heap->H[parent(pos)];
+		heap->H[parent(pos)] = temp;
+		V[heap->H[pos]->vertex].pos = parent(pos);
+		V[heap->H[parent(pos)]->vertex].pos = pos;
+		pos = parent(pos);
+	}
+	return 0;
+}
+void movingUp(HEAP* heap, int pos) {
+
+}
